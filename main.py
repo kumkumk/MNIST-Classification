458d1822584e35e103ec2c8e7289534456fdc8d1
Kumkum Kaushal <feddy15@gmail.com>
2019-01-03 21:14:50 -0500

    Add files via upload
    main executable file

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('Logistic')
+import LogisticRegression
+
+
+# In[1]:
+
+
+print('NeuralFull')
+import neuralFull
+
+
+# In[1]:
+
+
+print('NeuralKeras')
+import NeuralKeras
+
+
+# In[2]:
+
+
+print('SVM')
+import SVM
+
+
+# In[3]:
+
+
+print('RandomForest')
+import RandomForest
+
+
+# In[5]:
+
+
+print('Ensemble')
+import Ensemble
+
